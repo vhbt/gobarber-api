8abc9726be098d35373c08c79fc2b6fd0bbd22b3
Vitor Hariel <vhbarauna@gmail.com>
2019-06-30 16:05:29 -0300

    backend gobarber

---
+++ src/app/jobs/CancellationMail.js
@@ -0,0 +1,34 @@
+import { format, parseISO } from 'date-fns';
+import ptbr from 'date-fns/locale/pt-BR';
+import Mail from '../../lib/Mail';
+
+class CancellationMail {
+  get key() {
+    return 'CancellationMail';
+  }
+
+  async handle({ data }) {
+    const { appointment } = data;
+
+    console.log('A fila executou');
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          parseISO(appointment.date),
+          "'dia' dd 'de' MMMM', às' H:mm'h'",
+          {
+            locale: ptbr,
+          }
+        ),
+      },
+    });
+  }
+}
+
+export default new CancellationMail();
